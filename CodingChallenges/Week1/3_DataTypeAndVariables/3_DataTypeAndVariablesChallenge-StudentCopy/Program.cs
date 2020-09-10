dd3342f7f7477169fe8755a767efd785105e7098
lgairrett <mrg0227@umo.edu>
2020-09-10 09:43:10 -0400

    Completed Coding Challenges

--- CodingChallenges/Week1/3_DataTypeAndVariables/3_DataTypeAndVariablesChallenge-StudentCopy/Program.cs
+++ CodingChallenges/Week1/3_DataTypeAndVariables/3_DataTypeAndVariablesChallenge-StudentCopy/Program.cs
@@ -1,1 +1,63 @@
+using System;
+namespace _3_DataTypeAndVariablesChallenge
+{
+    public class Program
+    {
+      public static void Main(string[] args)
+      {
+          Console.WriteLine("Hello World!");
 
+          byte myByte = 25;
+          Console.WriteLine(myByte);
+
+          sbyte mySbyte = -15;
+          Console.WriteLine(mySbyte);
+
+          int myInt = -12318925;
+          Console.WriteLine(myInt);
+
+          uint myUint = 123091;
+          Console.WriteLine(myUint);
+
+          short myShort = -32768;
+          Console.WriteLine(myShort);
+
+          ushort myUShort = 65535;
+          Console.WriteLine(myUShort);
+
+          long myLong = -9223372036854775808;
+          Console.WriteLine(myLong);
+
+          ulong myULong = 18446744073709551615;
+          Console.WriteLine(myULong);
+
+          float myFloat = -31.1289f;
+          Console.WriteLine(myFloat);
+
+          double myDouble = -12.1231250;
+          Console.WriteLine(myDouble);
+
+          char myCharacter = 'A';
+          Console.WriteLine(myCharacter);
+
+          bool myBool = true;
+          Console.WriteLine(myBool);
+
+          string myText = "I control text";
+          Console.WriteLine(myText);
+
+          string numText = "15";
+          Console.WriteLine(Text2Num(numText));
+
+          decimal myDecimal = 20.13m;
+          Console.WriteLine(myDecimal);
+      }
+
+      public static int Text2Num(string numText)
+      {
+        // int number;
+        bool state = int.TryParse(numText, out int number);
+        return number;
+      }
+    }
+}
--- CodingChallenges/Week1/4_Methods/4_MethodsChallenge-StudentCopy/Program.cs
+++ CodingChallenges/Week1/4_Methods/4_MethodsChallenge-StudentCopy/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace _4_MethodsChallenge
 {
     public class Program
@@ -18,35 +17,92 @@ namespace _4_MethodsChallenge
 
             System.Console.WriteLine($"The result of your mathematical operation is {result3}.");
 
-
         }
 
         public static string GetName()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("What is your name?");
+            string newName = Console.ReadLine();
+            return newName;
+            //throw new NotImplementedException();
+
         }
 
         public static void GreetFriend(string name)
         {
             //Greeting should be: Hello, nameVar. You are my friend
             //Ex: Hello, Jim. You are my friend
-            throw new NotImplementedException();
+            Console.WriteLine($"Hello, {name}. You are my friend");
+            return;    
+            //throw new NotImplementedException();
         }
-
         public static double GetNumber()
         {
             //Should throw FormatException if the user did not input a number
-            throw new NotImplementedException();
+            Console.WriteLine("Please input a number");
+            string test = Console.ReadLine();
+            double num;
+        
+            if(Double.TryParse(test, out num))
+            {
+                num = double.Parse(test);
+                return num;
+            }
+            else
+            throw new FormatException();
+            //throw new NotImplementedException();
         }
 
         public static int GetAction()
         {
-            throw new NotImplementedException();
-        }
+            int result;
+            bool test;
+
+            do
+            {
+            Console.WriteLine("Please input an integer for the corresponding acition:\n1)add\n2)subtract\n3)multiply\n4)divide");
+            string response = Console.ReadLine();
+            test = int.TryParse(response, out result);
+
+            } while(!(result > 0 && result < 5) || test == false);
+
+        return result;
+        }   
 
         public static double DoAction(double x, double y, int z)
         {
-            throw new NotImplementedException();
+            double final;
+            if (z == 1){
+
+                final = x + y;
+                return final;
+            }
+
+            else if (z == 2){
+
+                final = y - x;
+                return final;
+            }
+
+            else if (z == 3){
+
+                final = x * y;
+                return final;
+            }
+
+            else if (z == 4){
+
+                if (x ==0){
+                    throw new FormatException();
+                }
+
+                final = y/x;
+                return final;
+            }
+
+            else 
+
+        throw new FormatException();
         }
     }
 }
