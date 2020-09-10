3ad455e527cc92ae6c55f633ae15ecd1209ca143
lgairrett <mrg0227@umo.edu>
2020-09-10 09:37:07 -0400

    Completed Coding Challenges

--- CodingChallenges/Week1/1_StringManipulationChallenge - StudentCopy/StringManipulationChallenge/StringManipulationChallenge.cs
+++ CodingChallenges/Week1/1_StringManipulationChallenge - StudentCopy/StringManipulationChallenge/StringManipulationChallenge.cs
@@ -47,121 +47,62 @@ namespace StringManipulationChallenge
             string upper = x.ToUpper();
             return upper;
         }
+
         // This method has one string parameter. 
         // It will:
         // 1) change the string to all lower case, 
         // 2) print the result to the console and 
         // 3) return the new string.        
         public static string StringToLower(string x){
-
             string lower = x.ToLower();
-
             return lower;
-
         }
-
-        
 
         // This method has one string parameter. 
-
         // It will:
-
         // 1) trim the whitespace from before and after the string, 
-
         // 2) print the result to the console and 
-
         // 3) return the new string.
-
         public static string StringTrim(string x){
-
             string trimed = x.Trim();
-
             return trimed;
-
         }
-
-        
 
         // This method has two parameters, one string and one integer. 
-
         // It will:
-
         // 1) get the substring based on the integer received, 
-
         // 2) print the result to the console and 
-
         // 3) return the new string.
-
         public static string StringSubstring(string x, int elementNum){
-
             string sub = x.Substring(elementNum);
-
             return sub;
-
         }
 
-
-
         // This method has two parameters, one string and one char.
-
         // It will:
-
         // 1) search the string parameter for the char parameter
-
         // 2) return the index of the char.
-
         public static int SearchChar(string userInputString, char x){
-
             int count = 0;
-
-
-
             foreach (var item in userInputString)
-
             {
-
                 count++;
-
-
-
                 if(item.Equals(x)){
-
                     break;
-
                 }
-
             }
-
-             
-
             return count-1;
 
         }
 
-
-
         // This method has two string parameters.
-
-        // It will:
-
+        // It will.
         // 1) concatenate the two strings with a space between them.
-
         // 2) return the new string.
-
         public static string ConcatNames(string fName, string lName){
-
             string firstAndLast = fName + " " + lName;
-
             return firstAndLast;
-
         }
 
-
-
-
-
-
-
     }//end of program
-
 }
