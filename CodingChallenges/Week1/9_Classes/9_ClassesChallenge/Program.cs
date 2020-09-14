e3cefd71b01ab2e3c597fd4fd00cbe38c3a4d103
lgairrett <mrg0227@umo.edu>
2020-09-14 08:20:25 -0400

    Week 1 challenges complete

--- CodingChallenges/Week1/9_Classes/9_ClassesChallenge/Program.cs
+++ CodingChallenges/Week1/9_Classes/9_ClassesChallenge/Program.cs
@@ -6,6 +6,24 @@ namespace _9_ClassesChallenge
     {
         public static void Main(string[] args)
         {
+            Human h1 = new Human();
+            Human h2 = new Human("Sam", "Murphy");
+           
+            Human h3 = new Human("Trevor", "Berk", "Green");
+            Human h4 = new Human("James", "Lang", 22);
+            Human h5 = new Human("Jaelyn", "Brown", "Brown", 21);
+
+            h1.Weight = 150;
+            h2.Weight = 200;
+            h3.Weight = 175;
+            h4.Weight = 185;
+            h5.Weight = 135;
+
+            h1.AboutMe();
+            h2.AboutMe();
+            h3.AboutMe();
+            h4.AboutMe();
+            h5.AboutMe();
         }
     }
 }
